57dab3cee14c3003410593e657191f243e091084
Ahmed Massoud <ahmed14massoud2021@gmail.com>
2025-08-23 19:18:55 +0300

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://mern-backend-production-b780.up.railway.app";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
